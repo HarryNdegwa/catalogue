4cfa6c4a56dd9df86b72b5fc2feab603d78e0c85
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-27 12:46:40 +0300

    Added coming soon middleware

---
+++ account/soon_middleware.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class ComingSoonMiddleware(object):
+
+    def __init__(self,get_response):
+        self.get_response = get_response
+
+
+    def __call__(self,request):
+        response = get_response(request)
+        return response
+
+
+    def process_view(self,request,view_func,view_args,view_kwargs):
+        return Response({},status=status.HTTP_200_OK)
