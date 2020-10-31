bcdc78b4e257bf33ff525d9ab4a9aab93a6b35e0
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-31 23:41:41 +0300

    Modified fetch user view

--- account/views.py
+++ account/views.py
@@ -84,7 +84,7 @@ class CurrentUser(APIView):
         email = request.user
         if email:
             user = User.objects.get(email=email)
-            return Response({"level":lambda user:2 if user.is_admin else 1},status=status.HTTP_200_OK)
+            return Response({},status=status.HTTP_200_OK)
         response = Response({},status=status.HTTP_401_UNAUTHORIZED)
         response.delete_cookie("_identity_")
         response.delete_cookie("lvl")
