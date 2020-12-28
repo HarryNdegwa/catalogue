58197bbdfd7cd0169161a4c3a0729b60bfcf4c82
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-28 21:32:32 +0300

    Removed dotenv in settings

--- api/settings.py
+++ api/settings.py
@@ -12,11 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 
 import os
-from dotenv import load_dotenv
 from datetime import timedelta
 from corsheaders.defaults import default_headers
 
-load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
