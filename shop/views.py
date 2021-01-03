b492b2d639c50c0f821e5f3992308e37b8606576
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-03 12:43:56 +0300

    refactored get orders view

--- shop/views.py
+++ shop/views.py
@@ -387,12 +387,13 @@ class OrderListCreateView(AdminBrowsableMixin,APIView,PaginationMixin):
     permission_classes = (permissions.IsAuthenticated,)
 
     def get(self,request,format=None):
-        q = Order.objects.all()
-        page = self.paginate_queryset(q)
+        q = Order.objects.filter(owner=request.user)
+        # page = self.paginate_queryset(q)
         serialized_q = OrderSerializer(q,many=True)
-        if page is not None:
-            serialized_q = OrderSerializer(page,many=True)
-            return self.get_paginated_response(serialized_q.data)
+        # if page is not None:
+        #     serialized_q = OrderSerializer(page,many=True)
+        #     return self.get_paginated_response(serialized_q.data)
+        print(serialized_q.data)
         return Response(serialized_q.data,status=status.HTTP_200_OK)
 
 
