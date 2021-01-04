8a7d0d58efef11ec9d4c7a87af14adc6fbfe637e
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-04 20:47:38 +0300

    Added fields to simple product serializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -89,7 +89,7 @@ class SimpleProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Product
-        fields=["id","name","price","prev_price","category","hot_deal","deal_price","img_urls","slug"]
+        fields=["id","name","price","prev_price","category","hot_deal","deal_price","img_urls","slug","reviews","sold"]
 
 
 
